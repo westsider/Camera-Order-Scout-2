f92e3eeb7863e8aa4c27bffaf5d84d35af210a55
Warren Hansen <whansen1@mac.com>
2017-04-06 16:21:32 -0700

    feat: added animation to demo

--- Camera Order Scout/DemoPhotoViewController.swift
+++ Camera Order Scout/DemoPhotoViewController.swift
@@ -22,6 +22,10 @@ class DemoPhotoViewController: UIViewController {
     
     @IBOutlet weak var bottomText: UILabel!
     
+    @IBOutlet weak var shareButton: UIButton!
+    
+    @IBOutlet weak var typeStack: UIStackView!
+    
     var counter = 0
     
     let feature1 = ["title":"Add Equipment","price":"move the selector","button":"press add","image":"1"]
@@ -52,71 +56,112 @@ class DemoPhotoViewController: UIViewController {
     @IBAction func leftSwipe(_ sender: UISwipeGestureRecognizer) {
         counter += 1
         if counter < 0 { counter = 0 }
-        if counter > 6 {counter = 6}
+        if counter > 7 {counter = 7}
         pageControl.currentPage += 1
         pageLoad(page: counter)
     }
     
     @IBAction func rightSwipe(_ sender: UISwipeGestureRecognizer) {
         counter -= 1
-        if counter > 6 {counter = 6}
+        if counter > 7 {counter = 7}
         pageControl.currentPage -= 1
         pageLoad(page: counter)
     }
     
+   
+    @IBAction func linkToFbbAction(_ sender: Any) {
+            if let url = NSURL(string: "https://www.facebook.com/iphoneCinematography/"){ UIApplication.shared.open(url as URL, options: [:], completionHandler: nil) }
+    }
+    
+    func letsDissolve(nextImage: String) {
+        let toImage = UIImage(named: nextImage)
+        UIView.transition(with: self.topImage,
+                          duration: 0.6,
+                          options: .transitionCrossDissolve,
+                          animations: {
+                            self.topImage.image = toImage
+        },
+                          completion: nil)
+    }
     
     func pageLoad(page: Int) {
         
         switch page {
         case 0:
-            topImage.image = UIImage(named: "demo_1")
+            topImage.isHidden = true
+            bottomImage.image = UIImage(named: "thumbsUp")
+            topText.text = "App Demo"
+            middleText.text = "• swipe left to explore"
+            bottomText.text = "• exit demo from title..."
+            shareButton.isHidden = true
+            typeStack.alignment = .leading
+        case 1:
+            topImage.isHidden = false
+            letsDissolve(nextImage: "demo_1")
+            //topImage.image = UIImage(named: "demo_1")
             bottomImage.image = UIImage(named: "1")
             topText.text = feature1["title"]
             middleText.text = feature1["price"]
             bottomText.text = feature1["button"]
-        case 1:
-            topImage.image = UIImage(named: "demo_2")
+            shareButton.isHidden = true
+        case 2:
+            letsDissolve(nextImage: "demo_2")
+            //topImage.image = UIImage(named: "demo_2")
             bottomImage.image = UIImage(named: "2")
             topText.text = feature2["title"]
             middleText.text = feature2["price"]
             bottomText.text = feature2["button"]
-        case 2:
-            topImage.image = UIImage(named: "demo_3")
+            shareButton.isHidden = true
+        case 3:
+            letsDissolve(nextImage: "demo_3")
+            //topImage.image = UIImage(named: "demo_3")
             bottomImage.image = UIImage(named: "3")
             topText.text = feature3["title"]
             middleText.text = feature3["price"]
             bottomText.text = feature3["button"]
-        case 3:
-            topImage.image = UIImage(named: "demo_4")
+            shareButton.isHidden = true
+        case 4:
+            letsDissolve(nextImage: "demo_4")
+            //topImage.image = UIImage(named: "demo_4")
             bottomImage.image = UIImage(named: "4")
             topText.text = feature4["title"]
             middleText.text = feature4["price"]
             bottomText.text = feature4["button"]
-        case 4:
-            topImage.image = UIImage(named: "demo_5")
+            shareButton.isHidden = true
+        case 5:
+            letsDissolve(nextImage: "demo_5")
+            //topImage.image = UIImage(named: "demo_5")
             bottomImage.image = UIImage(named: "5")
             topText.text = feature5["title"]
             middleText.text = feature5["price"]
             bottomText.text = feature5["button"]
-        case 5:
-            topImage.image = UIImage(named: "demo_6")
+            shareButton.isHidden = true
+        case 6:
+            topImage.isHidden = false
+            letsDissolve(nextImage: "demo_6")
+            //topImage.image = UIImage(named: "demo_6")
             bottomImage.image = UIImage(named: "6")
             topText.text = feature6["title"]
             middleText.text = feature6["price"]
             bottomText.text = feature6["button"]
-        case 6:
-            topImage.image = UIImage(named: "done")
+            shareButton.isHidden = true
+        case 7:
+            topImage.isHidden = true
+            letsDissolve(nextImage: "demo_7")
+            //topImage.image = UIImage(named: "done")
             bottomImage.image = UIImage(named: "thumbsUp")
-            topText.text = ""
-            middleText.text = "  Have a great shoot"
-            bottomText.text = ""
-
+            topText.text = "All Done!"
+            middleText.text = "• exit demo from title"
+            bottomText.text = "• tap 'i' to return here"
+            shareButton.isHidden = false
+            typeStack.alignment = .center
         default:
             topImage.image = UIImage(named: "demo_1")
             bottomImage.image = UIImage(named: "1")
             topText.text = feature1["title"]
             middleText.text = feature1["price"]
             bottomText.text = feature1["button"]
+            shareButton.isHidden = true
         }
     }
 }
