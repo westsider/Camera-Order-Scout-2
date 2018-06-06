aa3cbe23c87c3272da4466be4dd80f85d7a47c72
Warren Hansen <whansen1@me.com>
2018-06-06 10:18:36 -0500

    fix: keyboard on User info vc

--- Camera Order Scout/UserViewController.swift
+++ Camera Order Scout/UserViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import RealmSwift
+import IQKeyboardManagerSwift
 
 class UserViewController: UIViewController, UITextFieldDelegate {
     
@@ -52,8 +53,8 @@ class UserViewController: UIViewController, UITextFieldDelegate {
         //self.dateTextInput.delegate = self
         title = "J O B  I N F O"
         weatherDisplay.text = "\n\nEnter a City and State or Country below to get a 10 day weather forecast."
-        setupViewResizerOnKeyboardShown()
         setUpDatePicker()
+        IQKeyboardManager.shared.enable = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -67,23 +68,8 @@ class UserViewController: UIViewController, UITextFieldDelegate {
     }
     
     @IBAction func updateAction(_ sender: Any) {
-        
         updateRealm()
-        
         _ = navigationController?.popToRootViewController(animated: true)
-    }
-    
-    // MARK: - Keyboard behavior functions
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
-    }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        userName.resignFirstResponder()
-        production.resignFirstResponder()
-        company.resignFirstResponder()
-        dateTextInput.resignFirstResponder()
-        return true
     }
     
     //MARK: - Search Weather
@@ -199,46 +185,6 @@ class UserViewController: UIViewController, UITextFieldDelegate {
             currentEvent.weather = weatherDisplay.text
             currentEvent.tableViewArray[0].title = "\(userName.text!) Director of Photography"
             currentEvent.tableViewArray[0].detail = "Camera Order \(production.text!) \(dateTextInput.text!)"
-        }
-    }
-}
-
-extension UIViewController {
-    
-    func setupViewResizerOnKeyboardShown() {
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(UserViewController.keyboardWillShowForResizing),
-                                               name: Notification.Name.UIKeyboardWillShow,
-                                               object: nil)
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(UserViewController.keyboardWillHideForResizing),
-                                               name: Notification.Name.UIKeyboardWillHide,
-                                               object: nil)
-    }
-    
-    @objc func keyboardWillHideForResizing(notification: Notification) {
-        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
-            let viewHeight = self.view.frame.height
-            self.view.frame = CGRect(x: self.view.frame.origin.x,
-                                     y: self.view.frame.origin.y,
-                                     width: self.view.frame.width,
-                                     height: viewHeight + keyboardSize.height)
-        } else {
-            debugPrint("We're about to hide the keyboard and the keyboard size is nil. Now is the rapture.")
-        }
-    }
-    
-    @objc func keyboardWillShowForResizing(notification: Notification) {
-        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue,
-            let window = self.view.window?.frame {
-            // We're not just minusing the kb height from the view height because
-            // the view could already have been resized for the keyboard before
-            self.view.frame = CGRect(x: self.view.frame.origin.x,
-                                     y: self.view.frame.origin.y,
-                                     width: self.view.frame.width,
-                                     height: window.origin.y + window.height - keyboardSize.height)
-        } else {
-            debugPrint("We're showing the keyboard and either the keyboard size or window is nil: panic widely.")
         }
     }
 }
