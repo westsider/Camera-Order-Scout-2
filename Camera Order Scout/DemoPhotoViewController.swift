1cd8ad11349a9f89d50074ab3d1b574ffc3320c1
Warren Hansen <whansen1@me.com>
2018-06-08 10:43:17 -0500

    task: updated demo images

--- Camera Order Scout/DemoPhotoViewController.swift
+++ Camera Order Scout/DemoPhotoViewController.swift
@@ -14,8 +14,6 @@ class DemoPhotoViewController: UIViewController {
     
     @IBOutlet weak var topImage: UIImageView!
     
-    //@IBOutlet weak var bottomImage: UIImageView!
-    
     @IBOutlet weak var topText: UILabel!
     
     @IBOutlet weak var middleText: UILabel!
@@ -34,7 +32,7 @@ class DemoPhotoViewController: UIViewController {
     
     let feature3 = ["title":"Edit Job Details: Production, ect","price":"tap this row","button":"     ","image":"3"]
     
-    let feature4 = ["title":"Load a prior project","price":"press project","button":"","image":"4"]
+    let feature4 = ["title":"Load a prior project","price":"tap projects","button":"","image":"4"]
     
     let feature5 = ["title":"In Projects","price":"tap a row to load a prior project","button":"","image":"5"]
     
@@ -44,12 +42,8 @@ class DemoPhotoViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         title = "I N F O"
-        
         pageLoad(page: counter)
-        
-        //view.backgroundColor = coolGray
     }
     
 
@@ -89,7 +83,6 @@ class DemoPhotoViewController: UIViewController {
         switch page {
         case 0:
             topImage.isHidden = true
-            //bottomImage.image = UIImage(named: "infoDot")
             topText.text = "App Demo"
             middleText.text = "• swipe left to learn how this app works"
             bottomText.text = "• exit demo from title bar above"
@@ -98,58 +91,43 @@ class DemoPhotoViewController: UIViewController {
         case 1:
             topImage.isHidden = false
             letsDissolve(nextImage: "demo_1")
-            //topImage.image = UIImage(named: "demo_1")
-            //bottomImage.image = UIImage(named: "1")
             topText.text = feature1["title"]
             middleText.text = feature1["price"]
             bottomText.text = feature1["button"]
             shareButton.isHidden = true
         case 2:
             letsDissolve(nextImage: "demo_2")
-            //topImage.image = UIImage(named: "demo_2")
-           // bottomImage.image = UIImage(named: "2")
             topText.text = feature2["title"]
             middleText.text = feature2["price"]
             bottomText.text = feature2["button"]
             shareButton.isHidden = true
         case 3:
             letsDissolve(nextImage: "demo_3")
-            //topImage.image = UIImage(named: "demo_3")
-            //bottomImage.image = UIImage(named: "3")
             topText.text = feature3["title"]
             middleText.text = feature3["price"]
             bottomText.text = feature3["button"]
             shareButton.isHidden = true
         case 4:
             letsDissolve(nextImage: "demo_4")
-            //topImage.image = UIImage(named: "demo_4")
-           // bottomImage.image = UIImage(named: "4")
             topText.text = feature4["title"]
             middleText.text = feature4["price"]
             bottomText.text = feature4["button"]
             shareButton.isHidden = true
         case 5:
             letsDissolve(nextImage: "demo_5")
-            //topImage.image = UIImage(named: "demo_5")
-           // bottomImage.image = UIImage(named: "5")
             topText.text = feature5["title"]
             middleText.text = feature5["price"]
             bottomText.text = feature5["button"]
             shareButton.isHidden = true
         case 6:
-            //topImage.isHidden = false
             letsDissolve(nextImage: "demo_6")
-            //topImage.image = UIImage(named: "demo_6")
-            //bottomImage.image = UIImage(named: "6")
             topText.text = feature6["title"]
             middleText.text = feature6["price"]
             bottomText.text = feature6["button"]
             shareButton.isHidden = true
+        
         case 7:
-           // topImage.isHidden = true
             letsDissolve(nextImage: "demo_7")
-            //topImage.image = UIImage(named: "done")
-            //bottomImage.image = UIImage(named: "thumbsUp")
             topText.text = "All Done!"
             middleText.text = "• exit demo from title bar above"
             bottomText.text = "• tap 'i' to return here anytime"
@@ -157,7 +135,6 @@ class DemoPhotoViewController: UIViewController {
             typeStack.alignment = .center
         default:
             topImage.image = UIImage(named: "demo_1")
-            //bottomImage.image = UIImage(named: "1")
             topText.text = feature1["title"]
             middleText.text = feature1["price"]
             bottomText.text = feature1["button"]
